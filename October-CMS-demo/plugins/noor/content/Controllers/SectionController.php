e365d66908023aa30791c81b874964e784693c0f
nooraldeentumeh <90515044+Noortumeh@users.noreply.github.com>
2026-02-22 13:16:31 +0200

    add translating for response messages

--- October-CMS-demo/plugins/noor/content/Controllers/SectionController.php
+++ October-CMS-demo/plugins/noor/content/Controllers/SectionController.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace Noor\Content\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Lang;
 use Noor\Content\Resources\ContentResources;
 use Noor\Content\Services\SectionService;
 
@@ -21,9 +23,18 @@ class SectionController extends Controller
     public function homeContents(Request $request)
     {
         $sections = $this->sectionService->getHomeSections($request);
+
+        if ($sections->isEmpty()) {
+            return response()->json([
+                'success' => false,
+                'message' => Lang::get('noor.content::lang.no_sections_found') ?? 'No sections found',
+                'data' => null
+            ], 404);
+        }
+        
         return response()->json([
             'success' => true,
-            'message' => 'Sections retrieved successfully',
+            'message' => Lang::get('noor.content::lang.sections_retrieved_successfully'),
             'data' => ContentResources::collection($sections)
         ]);
     }
